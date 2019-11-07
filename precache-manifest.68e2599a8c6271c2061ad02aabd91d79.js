158ffc763066d77d00dd9afd4c02001fe489c972
cmilios <cmilios@logistic-i.gr>
2019-11-07 13:27:24 +0200

    Updates

---
+++ precache-manifest.68e2599a8c6271c2061ad02aabd91d79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63e0209b743ffefd2e14d8927ffedc5",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "685713dd7c535aef80dd",
+    "url": "/myportfolio/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b8dd06d99f8ec754b911",
+    "url": "/myportfolio/static/css/main.5b0c3bd2.chunk.css"
+  },
+  {
+    "revision": "685713dd7c535aef80dd",
+    "url": "/myportfolio/static/js/2.2f9a695f.chunk.js"
+  },
+  {
+    "revision": "b8dd06d99f8ec754b911",
+    "url": "/myportfolio/static/js/main.1465fbbe.chunk.js"
+  },
+  {
+    "revision": "60ddab2529cdc367f7e7",
+    "url": "/myportfolio/static/js/runtime-main.99d0ae82.js"
+  }
+]);
