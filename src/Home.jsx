d21e4c029cfcc3b0b60a48d14a9ce9ce3d59550d
cmilios <cmilios@logistic-i.gr>
2019-11-07 13:02:16 +0200

    added landscape image and welcome div

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,21 +3,21 @@ import PostList from "./PostList"
 import { posts } from "./dummy-posts";
 import { makeStyles } from '@material-ui/core/styles';
 import './index.css';
+import { Typography } from '@material-ui/core';
+import 'tachyons'
 
 
 const useStyles = makeStyles({
-    home:{
-        
-        backgroundImage:'url("https://source.unsplash.com/random/1520x720")',
-    }
+    
 
 })
 
 const Home = ()=>{
     const classes = useStyles();
     return(
-        <div className={classes.home}>
-            
+        <div className="home">
+
+            <Typography className="tc mwebfont " variant="h1" component="h1">Welcome</Typography>
             <PostList posts={posts} className={classes.list}/>
 
 
