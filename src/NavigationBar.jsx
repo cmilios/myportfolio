2f7b2287854526536e99e3b2ce64920f82ec8e22
Chris <cmilios@hotmail.com>
2019-11-19 16:24:30 +0200

    fill commit

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -48,17 +48,16 @@ export default function DenseAppBar() {
   };
 
   const sideList = side => (
-    <div
-      className={classes.list}
-      role="presentation"
-      onClick={toggleDrawer(side, false)}
-      onKeyDown={toggleDrawer(side, false)}
-    >
+    <div className={classes.list} role="presentation" 
+          onClick={toggleDrawer(side, false)} 
+          onKeyDown={toggleDrawer(side, false)}
+          >
       <List>
-        {['About', 'Disclaimer'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon ><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"><path d="M9 1C4.58 1 1 4.58 1 9s3.58 8 8 8 8-3.58 8-8-3.58-8-8-8zm0 2.75c1.24 0 2.25 1.01 2.25 2.25S10.24 8.25 9 8.25 6.75 7.24 6.75 6 7.76 3.75 9 3.75zM9 14.5c-1.86 0-3.49-.92-4.49-2.33C4.62 10.72 7.53 10 9 10c1.47 0 4.38.72 4.49 2.17-1 1.41-2.63 2.33-4.49 2.33z"/></svg></ListItemIcon>
-            <ListItemText primary={text} />
+        {[{text: 'About', icon: <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"><path d="M9 1C4.58 1 1 4.58 1 9s3.58 8 8 8 8-3.58 8-8-3.58-8-8-8zm0 2.75c1.24 0 2.25 1.01 2.25 2.25S10.24 8.25 9 8.25 6.75 7.24 6.75 6 7.76 3.75 9 3.75zM9 14.5c-1.86 0-3.49-.92-4.49-2.33C4.62 10.72 7.53 10 9 10c1.47 0 4.38.72 4.49 2.17-1 1.41-2.63 2.33-4.49 2.33z"/></svg>},
+          { text: 'Disclaimer', icon:<i class="material-icons"></i>}].map((x, index) => (
+          <ListItem button key={x.text}>
+            <ListItemIcon >{x.icon}</ListItemIcon>
+            <ListItemText primary={x.text} />
           </ListItem>
         ))}
       </List>
