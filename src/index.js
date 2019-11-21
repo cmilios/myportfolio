eb6cbcf2fdd05571a5555633d63dc342d2d216b3
cmilios <cmilios@logistic-i.gr>
2019-11-21 14:51:52 +0200

    added sentry

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/browser';
 import Home from "./Home"
+
+
+Sentry.init({dsn: "https://648b12265e4d4f0ca5277b253d613bfa@sentry.io/1829238"});
 
 ReactDOM.render(<Home/>, document.getElementById('root'));
 
